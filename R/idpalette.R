8f1e1bb8cfacce13619c60b8775ed61015dd57b3
geryan <geryan@users.noreply.github.com>
2024-05-10 15:46:31 +1000

    Oh everywhere

--- R/idpalette.R
+++ R/idpalette.R
@@ -29,11 +29,6 @@ idpalette <- function(
       )
     )
   } else {
-    warning(
-      sprintf(
-        "%s is greater than the number of colours in this palette (%s)\n Returning interpolated palette"
-      )
-    )
 
     idp <- grDevices::colorRampPalette(pal)(n)
 
@@ -46,7 +41,7 @@ idpalette <- function(
 #' @export
 #' @importFrom graphics rect par image text
 #' @importFrom grDevices rgb
-print.palette <- function(x, ...) {
+print.idpalette <- function(x, ...) {
   n <- length(x)
   old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
   on.exit(par(old))
@@ -54,6 +49,4 @@ print.palette <- function(x, ...) {
   image(1:n, 1, as.matrix(1:n), col = x,
         ylab = "", xaxt = "n", yaxt = "n", bty = "n")
 
-  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
-  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
 }
